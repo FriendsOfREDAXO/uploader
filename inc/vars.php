56d4466281c1f8b543752237ad666d18a43f60eb
ynamite <ynamite@bluewin.ch>
2025-06-11 19:28:46 +0200

    chore: improved formatting

--- inc/vars.php
+++ inc/vars.php
@@ -1,24 +1,24 @@
 <?php
-$allowed_filetypes = '""';	
-$args = rex_request('args','array');
-if(isset($args['types']) && trim($args['types'])) {
-	$allowed_filetypes = "/(\.|\/)(".implode('|',rex_mediapool_getMediaTypeWhitelist($args)).")$/i";
+$allowed_filetypes = '""';
+$args = rex_request('args', 'array');
+if (isset($args['types']) && trim($args['types'])) {
+    $allowed_filetypes = "/(\.|\/)(" . implode('|', rex_mediapool_getMediaTypeWhitelist($args)) . ")$/i";
 }
 return '
 <script>
 var uploader_options = {
     messages: {
-        maxNumberOfFiles: "'.rex_i18n::msg('uploader_errors_max_number_of_files').'",
-        acceptFileTypes: "'.rex_i18n::msg('uploader_errors_accept_file_types').'",
-        maxFileSize: "'.rex_i18n::msg('uploader_errors_max_file_size').'",
-        minFileSize: "'.rex_i18n::msg('uploader_errors_min_file_size').'"
+        maxNumberOfFiles: "' . rex_i18n::msg('uploader_errors_max_number_of_files') . '",
+        acceptFileTypes: "' . rex_i18n::msg('uploader_errors_accept_file_types') . '",
+        maxFileSize: "' . rex_i18n::msg('uploader_errors_max_file_size') . '",
+        minFileSize: "' . rex_i18n::msg('uploader_errors_min_file_size') . '"
     },
-    context: "'.$this->getProperty('context').'",
-    endpoint: "'.$this->getProperty('endpoint').'",
-    loadImageMaxFileSize: '.((int)$this->getConfig('image-max-filesize')*1000000).',
-    imageMaxWidth: '.(int)$this->getConfig('image-max-width').',
-    imageMaxHeight: '.(int)$this->getConfig('image-max-height').',
-    acceptFileTypes: '.$allowed_filetypes.'
+    context: "' . $this->getProperty('context') . '",
+    endpoint: "' . $this->getProperty('endpoint') . '",
+    loadImageMaxFileSize: ' . ((int)$this->getConfig('image-max-filesize') * 1000000) . ',
+    imageMaxWidth: ' . (int)$this->getConfig('image-max-width') . ',
+    imageMaxHeight: ' . (int)$this->getConfig('image-max-height') . ',
+    acceptFileTypes: ' . $allowed_filetypes . '
 };
 </script>
 ';
